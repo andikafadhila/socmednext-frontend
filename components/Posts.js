166038e2621079813cdb1dab43df465ddfc270de
andikafadhila <andikarizkifadhila@gmail.com>
2022-04-25 15:09:58 +0700

    tolong aku sheldaaaaaaa

--- components/Posts.js
+++ components/Posts.js
@@ -1,45 +1,85 @@
 import axios from "axios";
 import Post from "./Post";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import API_URL from "./apiurl";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function Posts() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(0);
+  const limit = 10;
+  console.log("posts", posts);
+  console.log("page", page);
+  // const fetchData = async () => {
+  //   try {
+  //     let res = await axios.get(`${API_URL}/post/get-post`);
+  //     setPosts(res.data);
+  //     // console.log("ini adalah res.data", res.data);
+  //     // console.log(
+  //     //   "ini adalah res.data.photos[0].image",
+  //     //   res.data[0].photos[0].image
+  //     // );
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  const fetchData = async () => {
+  const fetchDataOnScroll = async () => {
     try {
-      let res = await axios.get(`${API_URL}/post/get-post`);
-      setData(res.data);
-      console.log("ini adalah res.data", res.data);
-      console.log(
-        "ini adalah res.data.photos[0].image",
-        res.data[0].photos[0].image
-      );
+      const res = await axios.get(`${API_URL}/post/get-post`, {
+        params: { page, limit },
+      });
+
+      if (res.data.length === 0) setHasMore(false);
+      setPosts((prev) => [...prev, ...res.data]);
+      setPage((prev) => prev + 1);
     } catch (error) {
+      console.log("Error fetching Posts");
       console.log(error);
     }
   };
 
   useEffect(() => {
-    fetchData();
+    fetchDataOnScroll();
   }, []);
-  // console.log("ini data", data[0].photos[0].image);
-  // const alamatfoto = data[0].photos[0].image;
+
   return (
     <div>
-      {data.map((post) => (
-        <Post
-          key={post.id}
-          id={post.id}
-          username={post.username}
-          userImg={API_URL + post.profilepic}
-          dataimg={post.photos} //ini isinya object data foto post
-          numberOfLikes={post.number_of_likes}
-          caption={post.caption}
-        />
-      ))}
+      <InfiniteScroll
+        hasMore={hasMore}
+        next={fetchDataOnScroll}
+        loader={<div>Loading...</div>}
+        endMessage={<div>ga ada lagi bosku</div>}
+        dataLength={posts.length}
+      >
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            id={post.id}
+            username={post.username}
+            userImg={API_URL + post.profilepic}
+            dataimg={post.photos} //ini isinya object data foto post
+            numberOfLikes={post.number_of_likes}
+            caption={post.caption}
+          />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  try {
+    const res = await axios.get(`${API_URL}/post/get-post`, {
+      params: { page: 0 },
+    });
+
+    return { props: { postsData: res.data } };
+  } catch (error) {
+    return { props: { errorLoading: true } };
+  }
+};
 
 export default Posts;
